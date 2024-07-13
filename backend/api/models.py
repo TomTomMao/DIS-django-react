56276dae509ef1fef20040dd225bdd07cc534c8a
Wentao Mao <118118504+TomTomMao@users.noreply.github.com>
2024-07-13 16:46:36 +0800

    added Ownership, Offense, Incident and Fine Model

--- backend/api/models.py
+++ backend/api/models.py
@@ -6,20 +6,44 @@ from django.db import models
 # This Python class defines a Vehicle model with attributes for make, model, colour, and licence.
 class Vehicle(models.Model):
 
-    make = models.CharField(max_length=20, null=True)
-    model = models.CharField(max_length=20, null=True)
-    color = models.CharField(max_length=20, null=True)
-    plateNumber = models.CharField(max_length=7, null=True)
+    make = models.CharField(max_length=20, null=False)
+    model = models.CharField(max_length=20, null=False)
+    color = models.CharField(max_length=20, null=False)
+    plate_number = models.CharField(max_length=7, null=False)
 
 
 class People(models.Model):
-    name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50, null=False)
+    last_name = models.CharField(max_length=50, null=False)
     address = models.CharField(max_length=50, null=True)
     license = models.CharField(max_length=16, unique=True, null=True)
-    dob = models.DateField(null=True)
+    dob = models.DateField(verbose_name='date of birth', null=True)
 
 
 class Ownership(models.Model):
-    people = models.ForeignKey('People', on_delete=models.CASCADE)
-    vehicle = models.ForeignKey('Vehicle', on_delete=models.CASCADE)
+    people = models.ForeignKey('People', on_delete=models.PROTECT, null=True)
+    vehicle = models.ForeignKey(
+        'Vehicle', on_delete=models.PROTECT, null=False)
 
+
+class Offense(models.Model):
+    description = models.CharField(max_length=50, null=False)
+    max_fine = models.IntegerField(max_length=11, null=False)
+    max_points = models.IntegerField(max_length=11, null=False)
+
+
+class Incident(models.Model):
+    ownership = models.ForeignKey(
+        'Ownership', on_delete=models.PROTECT, null=True)
+    people = models.ForeignKey('People', on_delete=models.PROTECT, null=True)
+    offense = models.ForeignKey('Offense', on_delete=models.PROTECT)
+    creator = models.ForeignKey('User', on_delete=models.PROTECT, null=False)
+    date = models.DateField(null=False)
+    report = models.CharField(max_length=500, null=False)
+    
+
+class Fines(models.Model):
+    amount = models.IntegerField(max_length=20, null=False)
+    points = models.IntegerField(max_length=20, null=False)
+    incident = models.OneToOneField(
+        'Incident', on_delete=models.CASCADE, null=False)
