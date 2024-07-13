062971fd834a11c60dd964108fd96fcd1188b48a
Wentao Mao <118118504+TomTomMao@users.noreply.github.com>
2024-07-13 17:00:08 +0800

    registered admin website

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
-
+from api.models import Vehicle, People, Ownership, Offense, Incident, Fine
 # Register your models here.
+
+
+class VehicleAdmin(admin.ModelAdmin):
+    pass
+
+
+class PeopleAdmin(admin.ModelAdmin):
+    pass
+
+
+class OwnershipAdmin(admin.ModelAdmin):
+    pass
+
+
+class OffenseAdmin(admin.ModelAdmin):
+    pass
+
+
+class IncidentAdmin(admin.ModelAdmin):
+    pass
+
+
+class FineAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Vehicle, VehicleAdmin)
+admin.site.register(People, PeopleAdmin)
+admin.site.register(Ownership, OwnershipAdmin)
+admin.site.register(Offense, OffenseAdmin)
+admin.site.register(Incident, IncidentAdmin)
+admin.site.register(Fine, FineAdmin)
