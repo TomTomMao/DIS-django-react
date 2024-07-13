401669f8e89a141b20fbe516eb1a68d96c4382a1
Wentao Mao <118118504+TomTomMao@users.noreply.github.com>
2024-07-13 15:48:25 +0800

    added Vehicle, People, Ownership Model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+
+# This Python class defines a Vehicle model with attributes for make, model, colour, and licence.
+class Vehicle(models.Model):
+
+    make = models.CharField(max_length=20, null=True)
+    model = models.CharField(max_length=20, null=True)
+    color = models.CharField(max_length=20, null=True)
+    license = models.CharField(max_length=7, null=True)
+
+
+class People(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=50, null=True)
+    license = models.CharField(max_length=16, unique=True, null=True)
+    dob = models.DateField(null=True)
+
+
+class Ownership(models.Model):
+    people = models.ForeignKey('People', on_delete=models.CASCADE)
+    vehicle = models.ForeignKey('Vehicle', on_delete=models.CASCADE)
+
